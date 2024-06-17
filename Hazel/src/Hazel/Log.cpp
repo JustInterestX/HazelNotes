545a0a354bc68c2eaae8fde068f77e4a5db343da
JustInterestX <linjunjie200905@gmail.com>
2024-06-17 20:05:35 +0800

    Logging.

---
+++ Hazel/src/Hazel/Log.cpp
@@ -0,0 +1,22 @@
+#include "Log.h"
+
+#include "spdlog/sinks/stdout_color_sinks.h" 
+
+
+namespace Hazel {
+
+	std::shared_ptr<spdlog::logger> Log::s_Corelogger;
+	std::shared_ptr<spdlog::logger> Log::s_Clientlogger;
+
+	void Log::Init()
+	{
+		spdlog::set_pattern("%^[%T] %n: %v%$");
+
+		s_Corelogger = spdlog::stdout_color_mt("HAZEL");
+		s_Corelogger->set_level(spdlog::level::trace);
+
+		s_Clientlogger = spdlog::stdout_color_mt("APP");
+		s_Clientlogger->set_level(spdlog::level::trace);
+	}
+
+}
