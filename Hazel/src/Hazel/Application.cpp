00acae5f88167b1ff118fa96d4c180a5ce6ebfc2
JustInterestX <linjunjie200905@gmail.com>
2024-06-18 10:58:02 +0800

    Added layers and layer stack

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,8 +1,6 @@
 #include <hzpch.h>
 #include "Application.h"
 
-#include "Hazel/Log.h"
-
 #include <GLFW/glfw3.h>
 
 namespace Hazel {
@@ -20,13 +18,38 @@ namespace Hazel {
 
 	}
 
+	/*void Application::PushLayer(Layer* layer)
+	{
+		m_LayerStack.PushLayer(layer);
+	}
+
+	void Application::PushOverlay(Layer* overlayer)
+	{
+		m_LayerStack.PushOverlay(overlayer);
+	}*/
+	void Application::PushLayer(Layer* layer)
+	{
+		m_LayerStack.PushLayer(layer);
+	}
+
+	void Application::PushOverlay(Layer* layer)
+	{
+		m_LayerStack.PushOverlay(layer);
+	}
+
 	void Application::OnEvent(Event& e)
 	{
 		EventDispatcher dispatcher(e);
 		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
 
-
 		HZ_CORE_INFO("{0}", e);
+
+		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin(); )
+		{
+			(*--it)->OnEvent(e);
+			if (e.Handled)
+				break;
+		}
 	}
 
 	void Application::Run()
@@ -35,6 +58,10 @@ namespace Hazel {
 		{
 			glClearColor(1, 0, 1, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
+
+			for (Layer* layer : m_LayerStack)
+				layer->OnUpdate();
+
 			m_Window->OnUpdate();
 		}
 	}
--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -2,6 +2,8 @@
 
 #include "Core.h"
 #include "Window.h"
+#include "Hazel/LayerStack.h"
+#include "Hazel/Events/Event.h"
 #include "Hazel/Events/ApplicationEvent.h"
 
 namespace Hazel {
@@ -13,12 +15,18 @@ namespace Hazel {
 		virtual ~Application();
 
 		void Run();
+
 		void OnEvent(Event& e);
+
+		void PushLayer(Layer* layer);
+		void PushOverlay(Layer* layer);
 	private:
 		bool OnWindowClose(WindowCloseEvent& e);
 
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
+
+		LayerStack m_LayerStack;
 	};
 
 	// To be defined in CLIENT
